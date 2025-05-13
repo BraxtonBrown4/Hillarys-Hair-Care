fce73425b3f426a7cd3a18774994bbe500af6019
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-05-13 18:07:10 -0500

    adding api endpoints

--- Hillarys-Hair-Care-Server/Program.cs
+++ Hillarys-Hair-Care-Server/Program.cs
@@ -1,3 +1,4 @@
+using HillarysHairCare.models;
 using HillarysHairCare.models.DTOs;
 using Microsoft.EntityFrameworkCore;
 
@@ -37,28 +38,118 @@ app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
 
-app.MapGet("/stylists", (HillarysHairCareDbContext db) => {
+app.MapGet("/stylists", (HillarysHairCareDbContext db) =>
+{
     return db.Stylists
     .Include(s => s.StylistServices)
     .ThenInclude(ss => ss.Service)
-    .Select(stylist => new StylistDTO {
-            Id = stylist.Id,
-            Name = stylist.Name,
-            IsActive = stylist.IsActive,
-            Services = stylist.StylistServices.Select(s => new ServiceDTO {
-                Id = s.Service.Id,
-                Name = s.Service.Name,
-                Price = s.Service.Price
-            }).ToList()
+    .Select(stylist => new StylistDTO
+    {
+        Id = stylist.Id,
+        Name = stylist.Name,
+        IsActive = stylist.IsActive,
+        Services = stylist.StylistServices.Select(s => new ServiceDTO
+        {
+            Id = s.Service.Id,
+            Name = s.Service.Name,
+            Price = s.Service.Price
+        }).ToList()
     });
 });
 
-app.MapGet("/customers", (HillarysHairCareDbContext db) => {
-    return db.Customers.Select(customer => new CustomerDTO {
-            Id = customer.Id,
-            Name = customer.Name,
-            Balance = customer.Balance
+app.MapGet("/customers", (HillarysHairCareDbContext db) =>
+{
+    return db.Customers.Select(customer => new CustomerDTO
+    {
+        Id = customer.Id,
+        Name = customer.Name,
+        Balance = customer.Balance
     });
 });
+
+app.MapGet("/appointments", (HillarysHairCareDbContext db) =>
+{
+    return db.Appointments
+    .Include(a => a.Stylist)
+    .Include(a => a.Customer)
+    .Include(a => a.AppointmentServices)
+    .ThenInclude(a => a.Service)
+    .Select(appointment => new AppointmentDTO
+    {
+        Id = appointment.Id,
+        Date = appointment.Date,
+        Stylist = new StylistDTO
+        {
+            Id = appointment.Stylist.Id,
+            Name = appointment.Stylist.Name,
+            IsActive = appointment.Stylist.IsActive
+        },
+        Customer = new CustomerDTO
+        {
+            Id = appointment.Customer.Id,
+            Name = appointment.Customer.Name,
+            Balance = appointment.Customer.Balance
+        },
+        Services = appointment.AppointmentServices.Select(s => new ServiceDTO
+        {
+            Id = s.Service.Id,
+            Name = s.Service.Name,
+            Price = s.Service.Price
+        }).ToList()
+    });
+});
+
+app.MapPost("/appointments", (HillarysHairCareDbContext db, Appointment appointment) =>
+{
+    try
+    {
+        db.Appointments.Add(appointment);
+        db.SaveChanges();
+        
+        return Results.Created($"/appointments/{appointment.Id}", new AppointmentDTO {
+            Id = appointment.Id,
+            Date = appointment.Date,
+            StylistId = appointment.StylistId,
+            CustomerId = appointment.CustomerId
+        });
+    }
+    catch (DbUpdateException)
+    {
+        return Results.BadRequest("Invalid data submitted");
+    }
+});
+
+app.MapPut("/appointments/{id}", (HillarysHairCareDbContext db, int id, Appointment putA) => {
+    Appointment appointment = db.Appointments.FirstOrDefault(a => a.Id == id);
+
+    if (appointment == null) {
+        return Results.NotFound();
+    }
+
+    try
+    {
+        appointment.Date = putA.Date;
+        appointment.StylistId = putA.StylistId;
+        appointment.CustomerId = putA.CustomerId;
+
+        db.SaveChanges();
+
+        return Results.Ok(new AppointmentDTO
+        {
+            Id = appointment.Id,
+            Date = appointment.Date,
+            StylistId = appointment.StylistId,
+            CustomerId = appointment.CustomerId
+        });
+    }
+    catch (DbUpdateException)
+    {
+        return Results.BadRequest("Invalid data submitted");
+    }
+});
+
+
+
+//post service appointments
 
 app.Run();
--- Hillarys-Hair-Care-Server/models/appointment.cs
+++ Hillarys-Hair-Care-Server/models/appointment.cs
@@ -5,6 +5,10 @@ namespace HillarysHairCare.models;
 public class Appointment {
     public int Id {get; set;}
     public int CustomerId {get; set;}
+    public Customer? Customer {get; set;}
+    public Stylist? Stylist {get; set;}
     public int StylistId {get; set;}
     public DateTime Date {get; set;}
+    public List<AppointmentService>? AppointmentServices {get; set;}
+    
 }
